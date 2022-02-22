6579797c5402bfdf0128cb8e1a3704bfcd07fc1e
Lex <nrazam95@gmail.com>
2022-02-22 22:46:02 +0800

    trial queue

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { BullModule } from '@nestjs/bull';
+import { Module } from '@nestjs/common';
+import { AudioModule } from './audio/audio.module';
+
+@Module({
+  imports: [
+    BullModule.forRoot({
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+    AudioModule,
+  ],
+})
+export class AppModule {}
